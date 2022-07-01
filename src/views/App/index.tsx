d5d6e8d6285e555b6d118909f4aec6e16e76fb4a
Annie <anniehartmann97@gmail.com>
2022-07-02 00:24:30 +0300

    App done

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import styles from './index.module.scss';
+import { useToDoStore } from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/InputPlus';
+import { InputTask } from '../components/InputTask';
 
 type Props = {};
 
 export const App: React.FC = (props: Props) => {
+  const [tasks, createTask, updateTask, removeTask] = useToDoStore((state) => [
+    state.tasks,
+    state.createTask,
+    state.updateTask,
+    state.removeTask,
+  ]);
+
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
-      <section className={styles.articleSection}></section>
-      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}>
+        <InputPlus
+          onAdd={(title) => {
+            if (title) {
+              createTask(title);
+            }
+          }}
+        />
+      </section>
+      <section className={styles.articleSection}>
+        {!tasks.length && <p className={styles.articleText}>Нет задач</p>}
+        {tasks.map((task) => (
+          <InputTask
+            id={task.id}
+            title={task.title}
+            onDone={removeTask}
+            onEdited={updateTask}
+            onRemoved={removeTask}
+            key={task.id}
+          />
+        ))}
+      </section>
     </article>
   );
 };
---
+++ src/views/components/InputTask/index.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './index.module.scss';
+
+interface InputTaskProps {
+  id: string;
+  title: string;
+  onDone: (is: string) => void;
+  onEdited: (id: string, title: string) => void;
+  onRemoved: (id: string) => void;
+}
+
+export const InputTask: React.FC<InputTaskProps> = ({ id, title, onDone, onEdited, onRemoved }) => {
+  const [checked, setChecked] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [value, setValue] = useState(title);
+  const editTitleInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditMode) {
+      editTitleInputRef?.current?.focus();
+    }
+  }, [isEditMode]);
+
+  return (
+    <div className={styles.inputTask}>
+      <label className={styles.inputTaskLabel}>
+        <input
+          className={styles.inputTaskCheckbox}
+          type="checkbox"
+          disabled={isEditMode}
+          checked={checked}
+          onChange={(e) => {
+            setChecked(e.target.checked);
+            if (e.target.checked) {
+              setTimeout(() => {
+                onDone(id);
+              }, 300);
+            }
+          }}
+        />
+        {isEditMode ? (
+          <input
+            className={styles.inputTaskEditTitle}
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                onEdited(id, value);
+                setIsEditMode(false);
+              }
+            }}
+            ref={editTitleInputRef}
+          />
+        ) : (
+          <span className={styles.inputTaskTitle}>{title}</span>
+        )}
+      </label>
+      {isEditMode ? (
+        <button
+          className={styles.inputTaskSave}
+          aria-label="Save"
+          onClick={() => {
+            onEdited(id, value);
+            setIsEditMode(false);
+          }}
+        />
+      ) : (
+        <button
+          className={styles.inputTaskEdit}
+          aria-label="Edit"
+          onClick={() => {
+            setIsEditMode(true);
+          }}
+        />
+      )}
+      <button
+        className={styles.inputTaskRemove}
+        aria-label="Remove"
+        onClick={() => {
+          onRemoved(id);
+        }}
+      />
+    </div>
+  );
+};
